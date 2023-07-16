edc664aabeffd63a426bb30386030cbe10df7d16
albanijohanes <albanijohanes106@student.unsrat.ac.id>
2023-07-16 15:01:55 +0700

    bug carousel tag body

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,30 @@
-@section('title')
 @extends('layouts.layout')
-@extends('layouts.navbar')
+@include('layouts.navbar')
 @section('content')
-    <h1>page utama</h1>
+<div class="container mt-3">
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('assets/img/logoangkatan.png') }}" class="d-block w-100"
+                    alt="Logo">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('assets/img/logoangkatan.png') }}" class="d-block w-100"
+                    alt="Logo">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('assets/img/logoangkatan.png') }}" class="d-block w-100"
+                    alt="Logo">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
 @endsection
